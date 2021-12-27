4e5ee7855c3bc9dd3b9cb280346b0aa673d7d7d6
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-27 18:28:05 +0530

    first reactapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//function App() {
+  //return (
+   // <div className="App">
+     // <header className="App-header">
+      //  <img src={logo} className="App-logo" alt="logo" />
+       // <p>
+       //   Edit <code>src/App.js</code> and save to reload.
+       // </p>
+      //  <a
+        //  className="App-link"
+        //  href="https://reactjs.org"
+        //  target="_blank"
+         // rel="noopener noreferrer"
+        //>
+        //  Learn React
+       // </a>
+     // </header>
+   // </div>
+ // );
+//}
+
+//export default App;
+
+import React from "react";
+import "./index.css";
+export default function app(){
+  return(
+    <div className="app">
+      <h1 style={{color:"gray",textAlign:"justify"}}>hii everyone</h1> 
+      <p style={{padding:"50px",backgroundColor:"blue"}}>First react app</p>
     </div>
   );
 }
-
-export default App;
